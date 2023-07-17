96d9870b25951f24d246d6db012e8b1a80a5147d
Annad25 <anushkadebnath037@gmail.com>
2023-07-17 14:20:44 +0530

    django crud application
    crud application

--- BackOffice/wsgi.py
+++ BackOffice/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for assBackOffice project.
+WSGI config for BackOffice project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "assBackOffice.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BackOffice.settings")
 
 application = get_wsgi_application()
